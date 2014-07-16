127fefc6cfece711d8b8b1075f0888d61fdc6eab
Alexander Skulin <alexander.skulin@intexsoft.by>
2014-07-16 18:39:01 +0300

    Added request realization

---
+++ TutReader/NewsTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  NewsTableViewController.h
+//  TutReader
+//
+//  Created by crekby on 7/16/14.
+//  Copyright (c) 2014 crekby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsTableViewController : UITableViewController <NSURLConnectionDelegate,NSXMLParserDelegate>
+
+- (void) initOnlineNewsList;
+- (void) initFavoritesNewsList;
+
+@end
