dd870bd4d54ff1e908e0327a12fee7dde2521831
Alexander Skulin <alexander.skulin@intexsoft.by>
2014-07-16 23:45:10 +0300

    added custom cell class

---
+++ TutReader/UI/Cells/newsCell.h
@@ -0,0 +1,17 @@
+//
+//  newsCell.h
+//  TutReader
+//
+//  Created by crekby on 7/16/14.
+//  Copyright (c) 2014 crekby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface newsCell : UITableViewCell
+
+@property (nonatomic) IBOutlet UIImageView* imageView;
+@property (nonatomic) IBOutlet UILabel* newsTitle;
+@property (nonatomic) IBOutlet UILabel* newsDescription;
+
+@end
