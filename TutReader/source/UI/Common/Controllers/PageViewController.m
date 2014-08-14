fc6cfac90dd4973c9993071afffd79a5670dcfbd
Alexander Skulin <alexander.skulin@intexsoft.by>
2014-08-14 17:11:54 +0300

    added speech synthesiser to read news

--- TutReader/source/UI/Common/Controllers/PageViewController.m
+++ TutReader/source/UI/Common/Controllers/PageViewController.m
@@ -12,11 +12,14 @@
 #import "ShareViewController.h"
 #import "ShareManager.h"
 #import "FavoriteNewsManager.h"
+#import <AVFoundation/AVSpeechSynthesis.h>
+
 
 @interface PageViewController() <ShareViewControllerDelegate,UIPageViewControllerDelegate,UIPageViewControllerDataSource,UIPopoverControllerDelegate,UINavigationControllerDelegate>
 
 @property (nonatomic, retain) IBOutlet UIPopoverController *sharePopover;
 @property (nonatomic) UIBarButtonItem* favoriteBarButton;
+@property (nonatomic) AVSpeechSynthesizer *speechSynthesizer;
 
 @end
 
@@ -37,12 +40,13 @@
     self.favoriteBarButton = [[UIBarButtonItem alloc] initWithImage:starImage style:UIBarButtonItemStyleBordered target:self action:@selector(btnFavoriteDidTap:)];
     UIBarButtonItem* shareBarButton = [[UIBarButtonItem alloc] initWithTitle:@"" style:UIBarButtonItemStyleBordered target:self action:@selector(showPopover:)];
     shareBarButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAction target:self action:@selector(showPopover:)];
+    UIBarButtonItem* speechButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"speech"] style:UIBarButtonItemStyleBordered target:self action:@selector(speechButtonDidTap)];
     if (IS_IPAD) {
         if ([[DataProvider instance] selectedNews].newsURL) {
             self.title = [[DataProvider instance] selectedNews].newsTitle;
         }
     }
-    self.navigationItem.rightBarButtonItems = @[self.favoriteBarButton,shareBarButton];
+    self.navigationItem.rightBarButtonItems = @[self.favoriteBarButton,shareBarButton,speechButton];
     [[NSNotificationCenter defaultCenter] addObserver:self
                                              selector:@selector(orientationChangeNotificationAction)
                                                  name:UIDeviceOrientationDidChangeNotification
@@ -169,6 +173,100 @@
 }
 
 #pragma mark - Private methods
+
+- (void) speechButtonDidTap
+{
+    
+        NSString* html = [[self.viewControllers[0] webView] stringByEvaluatingJavaScriptFromString:@"document.body.innerHTML"];
+        unsigned long lenght = [html rangeOfString:@"id=\"article_body\""].location+18;
+    if (lenght > html.length) {
+        NSLog(@"%@",html);
+        return;
+    }
+        html = [html stringByReplacingCharactersInRange:NSMakeRange(0, lenght) withString:@""];
+        unsigned long location = [html rangeOfString:@"id=\"utm_theme_news_block\""].location;
+    if (location > html.length) {
+        location = [html rangeOfString:@"<div class=\"b-related\">"].location;
+        if (location>html.length) {
+            NSLog(@"%@",html);
+            return;
+        }
+        
+    }
+        html = [html stringByReplacingCharactersInRange:NSMakeRange(location, html.length - location) withString:@""];
+        html = [[[[[[[[[html stringByReplacingOccurrencesOfString:@"<div>" withString:@""]
+                     stringByReplacingOccurrencesOfString:@"</div>" withString:@""]
+                     stringByReplacingOccurrencesOfString:@"<br>" withString:@""]
+                     stringByReplacingOccurrencesOfString:@"<strong>" withString:@""]
+                     stringByReplacingOccurrencesOfString:@"</strong>" withString:@""]
+                     stringByReplacingOccurrencesOfString:@"<em>" withString:@""]
+                     stringByReplacingOccurrencesOfString:@"</em>" withString:@""]
+                     stringByReplacingOccurrencesOfString:@"&nbsp;" withString:@""]
+                     stringByReplacingOccurrencesOfString:@"</a>" withString:@""];
+        
+        html = [html stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        
+        while ([html rangeOfString:@"<img"].location != NSNotFound) {
+            unsigned long start = [html rangeOfString:@"<img"].location;
+            unsigned long end = [html rangeOfString:@">" options:NSLiteralSearch range:NSMakeRange(start, html.length-start)].location + 1;
+            if (start != NSNotFound && end != NSNotFound) {
+                html = [html stringByReplacingCharactersInRange:NSMakeRange(start, end-start) withString:@""];
+            }
+        }
+        
+        while ([html rangeOfString:@"<a"].location != NSNotFound) {
+            unsigned long start = [html rangeOfString:@"<a"].location;
+            unsigned long end = [html rangeOfString:@">" options:NSLiteralSearch range:NSMakeRange(start, html.length-start)].location + 1;
+            if (start != NSNotFound && end != NSNotFound) {
+                html = [html stringByReplacingCharactersInRange:NSMakeRange(start, end-start) withString:@""];
+            }
+        }
+        
+        while ([html rangeOfString:@"<table"].location != NSNotFound) {
+            unsigned long start = [html rangeOfString:@"<table"].location;
+            unsigned long end = [html rangeOfString:@"</table>" options:NSLiteralSearch range:NSMakeRange(start, html.length-start)].location + 8;
+            if (start != NSNotFound && end != NSNotFound) {
+                html = [html stringByReplacingCharactersInRange:NSMakeRange(start, end-start) withString:@""];
+            }
+        }
+        
+        while ([html rangeOfString:@"<div"].location != NSNotFound) {
+            unsigned long start = [html rangeOfString:@"<div"].location;
+            unsigned long end = [html rangeOfString:@">" options:NSLiteralSearch range:NSMakeRange(start, html.length-start)].location + 1;
+            if (start != NSNotFound && end != NSNotFound) {
+                if (end==2147483648) {
+                    end=html.length;
+                }
+                html = [html stringByReplacingCharactersInRange:NSMakeRange(start, end-start) withString:@""];
+            }
+        }
+        
+        unsigned long comments = [html rangeOfString:@"<!--"].location;
+        if (comments != NSNotFound) {
+            html = [html stringByReplacingCharactersInRange:NSMakeRange(comments, html.length-comments) withString:@""];
+        }
+
+        
+        NSCharacterSet *notAllowedChars = [[NSCharacterSet characterSetWithCharactersInString:@"йцукенгшщзхъфывапролджэёячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЁЯЧСМИТЬБЮ 1234567890 ,.:"] invertedSet];
+        html = [[html componentsSeparatedByCharactersInSet:notAllowedChars] componentsJoinedByString:@""];
+        NSLog(html);
+    //    NSRegularExpression *expression = [NSRegularExpression regularExpressionWithPattern:@"[,\\.`\"]"
+    //                                                                                options:0
+    //                                                                                  error:NULL];
+    //    NSString *sampleString = @"The \"new\" quick brown fox, who jumped over the lazy dog.";
+    //    NSString *cleanedString = [expression stringByReplacingMatchesInString:sampleString
+    //                                                                   options:0
+    //                                                                     range:NSMakeRange(0, sampleString.length)
+    //                                                              withTemplate:@""];
+            //NSString *string = @"Активный атмосферный фронт, смещающийся с юго-запада Европы, который в утренние часы принес в Минск дожди, ушел из столицы. Вечером Минск окажется под влиянием еще одного активного атмосферного фронта с территории юго-западной Европы. Он также принесет с собой сильные ливни. Об этом сообщает Республиканский гидрометеоцентр.";
+            AVSpeechUtterance *utterance = [[AVSpeechUtterance alloc] initWithString:html];
+        if (self.speechSynthesizer.speaking) {
+            [self.speechSynthesizer stopSpeakingAtBoundary:AVSpeechBoundaryImmediate];
+        }
+            self.speechSynthesizer = [[AVSpeechSynthesizer alloc] init];
+            utterance.rate = 0.2f;
+            [self.speechSynthesizer speakUtterance:utterance];
+}
 
 - (void)setupNews
 {
