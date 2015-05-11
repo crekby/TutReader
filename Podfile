33fbbaaf168e054a35d49b35a56ee4ff000425ae
Alexander Skulin <alexander.skulin@intexsoft.by>
2015-05-11 12:37:51 +0300

    added core plot via cocoa pods

--- Podfile
+++ Podfile
@@ -2,3 +2,5 @@
 pod 'GoogleAnalytics-iOS-SDK', '~> 3.0.6'
 
 pod 'google-plus-ios-sdk', '~> 1.5.0'
+
+pod 'CorePlot'
--- TutReader/source/UI/Common/Controllers/GraphController.h
+++ TutReader/source/UI/Common/Controllers/GraphController.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "CorePlot-CocoaTouch.h"
 
 @interface GraphController : UIViewController <CPTPlotDataSource>
 
